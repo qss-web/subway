4f7f730c83872dbc0604a26cea2f9514252d678f
qiangshushu <qiang@futengwl.com>
2018-03-08 18:10:22 +0800

    qss

---
+++ src/components/train.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="train-list flex">
+        <div class="train-body flex">
+            <span class="train-name">6号线<br />Line6</span>
+            <span v-if="!select" class="el-dropdown-link">田村站</span>
+            <el-dropdown v-else class="station-select" placement="top-start">
+                <span class="el-dropdown-link">田村站</span>
+                <el-dropdown-menu class="station-list" slot="dropdown">
+                    <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名一</el-dropdown-item>
+                    <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名二</el-dropdown-item>
+                    <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名三</el-dropdown-item>
+                    <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名四</el-dropdown-item>
+                    <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名五</el-dropdown-item>
+                    <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名六</el-dropdown-item>
+                </el-dropdown-menu>
+            </el-dropdown>
+        </div>
+        <div class="train-header"></div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        props: ['select']
+    };
+</script>
+
+
+<style lang="less" scoped>
+    .train-list {
+        position: absolute;
+        overflow: hidden;
+        height: 0.54rem;
+        bottom: 0.95rem;
+        left: 0;
+        width: 4.21rem;
+        .train-body {
+            background: url('~assets/siteInfo/bg_train_body.png') no-repeat;
+            background-size: 100% 100%;
+            height: 100%;
+            flex: auto;
+            justify-content: flex-end;
+            .train-name {
+                color: #fff;
+                font-size: 0.21rem;
+            }
+        }
+        .train-header {
+            background: url('~assets/siteInfo/icon_subway_header.png') no-repeat right top;
+            width: 1.16rem;
+            height: 100%;
+        }
+    }
+
+    .el-dropdown-link {
+        color: #fff;
+        font-size: 0.24rem;
+        padding: 0 0.3rem;
+    }
+
+    .station-list {
+        border: 0;
+        padding: 0;
+        li {
+            color: #fff;
+            background-color: #6e7488;
+            font-size: 0.21rem;
+            padding: 0 0.2rem;
+        }
+    }
+</style>
--- src/views/siteInfo.vue
+++ src/views/siteInfo.vue
@@ -1,43 +1,53 @@
 <template>
-    <div class="">
+    <div>
         <div>
-            <div class="station-3d"></div>
-            <div class="station-healthy"></div>
+            <div class="device-3d">
+                <v-tag type="error" x="2.1" y="2.6">1#屏蔽门</v-tag>
+                <v-tag type="error" x="3" y="4">1#屏蔽门</v-tag>
+                <v-tag type="error" x="2.9" y="4.82">2#屏蔽门</v-tag>
+                <v-tag type="error" x="1" y="2">3#屏蔽门</v-tag>
+                <v-tag type="error" x="1" y="2">A2屏蔽门</v-tag>
+                <v-tag type="error" x="1" y="2">A3屏蔽门</v-tag>
+                <v-tag type="error" x="1" y="2">4#屏蔽门</v-tag>
+                <v-tag type="normal" x="1" y="2">A2扶梯</v-tag>
+                <v-tag type="normal" x="1" y="2">B1扶梯</v-tag>
+                <v-tag type="normal" x="1" y="2">B2扶梯</v-tag>
+                <v-tag type="normal" x="1" y="2">B3扶梯</v-tag>
+                <v-tag type="normal" x="1" y="2">B4扶梯</v-tag>
+                <v-tag type="normal" x="1" y="2">B5扶梯</v-tag>
+                <v-tag type="normal" x="1" y="2">C1扶梯</v-tag>
+                <v-tag type="normal" x="1" y="2">C2扶梯</v-tag>
+            </div>
+            <div class="device-healthy">
+                <button class="device-healthy-title">今日车站健康监测指标</button>
+                <div class="device-healthy-body">
+                    <div class="healthy-charts flex">
+                        <v-health-indicators class="healthy-chart" id="health1" title="自动扶梯" :percent="68"></v-health-indicators>
+                        <v-health-indicators class="healthy-chart" id="health2" title="风机" :percent="68"></v-health-indicators>
+                        <v-health-indicators class="healthy-chart" id="health3" title="站台门" :percent="68"></v-health-indicators>
+                    </div>
+                    <div class="healthy-table">1122</div>
+                </div>
+            </div>
             <div class="fans flex">
                 <div class="fan flex">
                     <img class="fan-icon" src="~assets/siteInfo/icon_fan.png" />
-                    <button class="fan-name error">F01风机</button>
+                    <button class="fan-name error" @click="gotoFan">F01风机</button>
                 </div>
                 <div class="fan flex">
                     <img class="fan-icon" src="~assets/siteInfo/icon_fan.png" />
-                    <button class="fan-name warn">F02风机</button>
+                    <button class="fan-name warn" @click="gotoFan">F02风机</button>
                 </div>
                 <div class="fan flex">
                     <img class="fan-icon" src="~assets/siteInfo/icon_fan.png" />
-                    <button class="fan-name normal">F03风机</button>
+                    <button class="fan-name normal" @click="gotoFan">F03风机</button>
                 </div>
                 <div class="fan flex">
                     <img class="fan-icon" src="~assets/siteInfo/icon_fan.png" />
-                    <button class="fan-name error">F04风机</button>
+                    <button class="fan-name error" @click="gotoFan">F04风机</button>
                 </div>
             </div>
-            <div class="train-list flex">
-                <div class="train-body flex">
-                    <span class="train-name">6号线<br />Line6</span>
-                    <el-dropdown class="station-select" placement="top-start">
-                        <span class="el-dropdown-link">田村站</span>
-                        <el-dropdown-menu class="station-list" slot="dropdown">
-                            <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名一</el-dropdown-item>
-                            <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名二</el-dropdown-item>
-                            <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名三</el-dropdown-item>
-                            <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名四</el-dropdown-item>
-                            <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名五</el-dropdown-item>
-                            <el-dropdown-item><img src="~assets/siteInfo/icon_subway_mini.png" /> 站名六</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                </div>
-                <div class="train-header"></div>
-            </div>
+            <v-train :select="true"></v-train>
         </div>
         <v-goback></v-goback>
     </div>
@@ -45,18 +55,16 @@
 
 <script>
     export default {
-        data() {
-            return {};
-        },
-        props: [],
-        created() { },
-        computed: {},
-        methods: {}
+        methods: {
+            gotoFan() {
+                this.$router.push('faninfo');
+            }
+        }
     };
 </script>
 
 <style lang="less" scoped>
-    .station-3d {
+    .device-3d {
         position: absolute;
         left: -0.1rem;
         top: 0.75rem;
@@ -65,7 +73,8 @@
         background: url('~assets/siteInfo/bg_station.png') no-repeat;
         background-size: 100% 100%;
     }
-    .station-healthy {
+
+    .device-healthy {
         position: absolute;
         top: 0.26rem;
         right: 0.26rem;
@@ -73,7 +82,36 @@
         height: 8.95rem;
         background: url('~assets/siteInfo/bg_healthy.png') no-repeat;
         background-size: 100% 100%;
+        &-title {
+            background: url('~assets/siteInfo/bg_title.png') no-repeat;
+            width: 2.7rem;
+            height: 0.44rem;
+            color: #cfd6f0;
+            font-size: 0.22rem;
+            position: absolute;
+            left: 0.34rem;
+            top: 0.12rem;
+            padding-bottom: 0.04rem;
+        }
+        &-body {
+            width: 8.24rem;
+            height: 7.9rem;
+            border-radius: 0.2rem;
+            background-color: #404455;
+            position: absolute;
+            left: 0.31rem;
+            top: 0.6rem;
+            .healthy-charts {
+                height: 2.68rem;
+                justify-content: space-around;
+                padding: 0 0.3rem;
+            }
+            .healthy-table {
+                background-color: #000;
+            }
+        }
     }
+
     .fans {
         position: absolute;
         flex-wrap: wrap;
@@ -104,48 +142,6 @@
                     background-color: #36ff00;
                 }
             }
-        }
-    }
-    .train-list {
-        position: absolute;
-        overflow: hidden;
-        height: 0.54rem;
-        bottom: 0.95rem;
-        left: 0;
-        width: 4.21rem;
-        .train-body {
-            background: url('~assets/siteInfo/bg_train_body.png') no-repeat;
-            background-size: 100% 100%;
-            height: 100%;
-            flex: auto;
-            justify-content: flex-end;
-            .train-name {
-                color: #fff;
-                font-size: 0.21rem;
-            }
-            .station-select {
-                padding: 0 0.3rem;
-            }
-        }
-        .train-header {
-            background: url('~assets/siteInfo/icon_subway_header.png') no-repeat right top;
-            width: 1.16rem;
-            height: 100%;
-        }
-    }
-
-    .el-dropdown-link {
-        color: #fff;
-        font-size: 0.24rem;
-    }
-    .station-list {
-        border: 0;
-        padding: 0;
-        li {
-            color: #fff;
-            background-color: #6e7488;
-            font-size: 0.21rem;
-            padding: 0 0.2rem;
         }
     }
 </style>
